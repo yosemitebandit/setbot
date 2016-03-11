b36e0ff4f21ee587e81f816407c8b7e0245d6ac8
Matt Ball <matt.ball.2@gmail.com>
2016-03-11 13:21:16 -0800

    sel adjustments

--- selenium_test.py
+++ selenium_test.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 
 driver = webdriver.PhantomJS(service_log_path='/dev/null')
-driver.set_window_size(1024, 768)
+driver.set_window_size(400, 600)
 driver.get('http://localhost:8000/svg-cards')
 driver.save_screenshot('screenshot.png')
+
+driver.exit()
 
 '''
 # can't get this to work :/
