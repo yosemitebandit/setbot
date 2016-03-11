992129124be332bfd6db67ddd2d5c34119c3f594
Matt Ball <matt.ball.2@gmail.com>
2016-03-10 18:24:06 -0800

    adds a solid, one-path bean

--- svg-cards/cards-with-snap.js
+++ svg-cards/cards-with-snap.js
@@ -19,50 +19,43 @@ cardBackground.attr({
 });
 
 // two red striped beans
-var topHalfBean = paper.path(
-  'M 280 300 ' +
-  'c 0 0, 10 -60, 50 -60 ' +
-  'c 40 0, 85 20, 125 20 ' +
-  'c 95 0, 140 -70, 110 40 '
+var bean = paper.path(
+  'M 29, 104 ' +
+  'C 22 140, 29 159, 37 172 ' +
+  'C 49 191, 70 190, 93 172 ' +
+  'C 124 148, 165 154, 178 162' +
+  'C 212 182, 261 178, 276 174' +
+  'C 310 165, 331 137, 338 124' +
+  'C 352 98, 351 57, 339 45 ' +
+  'C 316 22, 288 58, 266 63 ' +
+  'C 246 67, 228 72, 205 63 ' +
+  'C 181 54, 137 39, 122 38 ' +
+  'C 89 36, 69 50, 57 59' +
+  'C 43 69, 37 84, 30 102 ' +
+  'z'
 );
-var beanScaleFactor = 0.8;
-var topMatrix = new Snap.Matrix();
-topMatrix.scale(beanScaleFactor);
-topMatrix.translate(100, 0);
-topHalfBean.transform(topMatrix);
-
-var bottomHalfBean = topHalfBean.clone();
-var bottomMatrix = new Snap.Matrix();
-bottomMatrix.scale(beanScaleFactor);
-bottomMatrix.rotate(180, 400, 300);
-bottomMatrix.translate(-145, 0);
-bottomHalfBean.transform(bottomMatrix);
+var beanScaleFactor = 0.4;
+var beanMatrix = new Snap.Matrix()
+beanMatrix.scale(beanScaleFactor);
+beanMatrix.translate(300, 0);
+bean.transform(beanMatrix);
 
 // create the pattern
 var patternPath = (
   'M 10 -10 ' +
   'L 10 10 '
 );
-// magic numbers for good alignment with 10,-10 -> 10,10: 11 and 15
-var inversePatternDensity = 11;
 var stripedPattern = paper.path(patternPath).attr({
   fill: 'none',
   stroke: 'red',
   strokeWidth: 1,
-}).toPattern(0, 0, inversePatternDensity, 10);
+}).toPattern(0, 0, 10, 10);
 
-// group the top and bottom half together
-var bean = paper.g(topHalfBean, bottomHalfBean);
 bean.attr({
   fill: stripedPattern,
   stroke: 'red',
   strokeWidth: 3,
 });
-
-// move the whole thing
-var beanMatrix = new Snap.Matrix();
-beanMatrix.translate(0, -30);
-bean.transform(beanMatrix);
 
 // make a second bean and move it in place
 var secondBean = bean.clone();
