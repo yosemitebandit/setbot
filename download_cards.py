9dc543cffd0617f3c6e1e4c596095547bec82e81
Matt Ball <matt.ball.2@gmail.com>
2016-03-11 16:42:12 -0800

    script downloads all images of cards

--- download_cards.py
+++ download_cards.py
@@ -1,24 +1,44 @@
+"""Downloads images of all cards.
+"""
+
 import os
 import urllib
-import urlparse
 
 from selenium import webdriver
 
 
-base_url = 'http://localhost:8000/svg-cards'
-params = {
-  'number': 2,
-  'color': 'purple',
-  'texture': 'solid',
-  'shape': 'diamond',
-}
-url_params = urllib.urlencode(params)
-url = '%s?%s' % (base_url, url_params)
-
+# Setup the webdriver and output path.
 driver = webdriver.PhantomJS(service_log_path='/dev/null')
 driver.set_window_size(800, 650)
-driver.get(url)
-driver.save_screenshot('screenshot.png')
+base_url = 'http://localhost:8000/svg-cards'
+out_path = 'card-images'
+if not os.path.exists(out_path):
+  os.makedirs(out_path)
+
+# List all valid params.
+numbers = (1, 2, 3)
+colors = ('red', 'green', 'purple')
+textures = ('solid', 'empty', 'striped')
+shapes = ('oval', 'diamond', 'bean')
+
+# Capture an image for each permutation.
+for number in numbers:
+  for color in colors:
+    for texture in textures:
+      for shape in shapes:
+        params = {
+          'number': number,
+          'color': color,
+          'texture': texture,
+          'shape': shape,
+        }
+        url_params = urllib.urlencode(params)
+        url = '%s?%s' % (base_url, url_params)
+        driver.get(url)
+        filename = '%s-%s-%s-%s.png' % (number, color, texture, shape)
+        print filename
+        save_path = os.path.join(out_path, filename)
+        driver.save_screenshot(save_path)
 
 # The driver.close and .quit methods aren't working..
 os.system('pkill phantomjs')
