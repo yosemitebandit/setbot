08b146bc419ad1659ee933db52e2a07963446b32
Matt Ball <matt.ball.2@gmail.com>
2016-03-11 16:11:18 -0800

    capturing card images

--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,24 @@
 import os
+import urllib
+import urlparse
 
 from selenium import webdriver
 
 
+base_url = 'http://localhost:8000/svg-cards'
+params = {
+  'number': 2,
+  'color': 'purple',
+  'texture': 'solid',
+  'shape': 'diamond',
+}
+url_params = urllib.urlencode(params)
+url = '%s?%s' % (base_url, url_params)
+
 driver = webdriver.PhantomJS(service_log_path='/dev/null')
 driver.set_window_size(800, 650)
-driver.get('http://localhost:8000/svg-cards')
+driver.get(url)
 driver.save_screenshot('screenshot.png')
 
 # The driver.close and .quit methods aren't working..
 os.system('pkill phantomjs')
-
-
-'''
-# can't get this to work :/
-body = driver.find_element_by_tag_name('body')
-print body
-body.send_keys('s')
-'''
