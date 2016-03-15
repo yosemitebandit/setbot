462c24cda62646305dcbe2246049cf46178ec33a
Matt Ball <matt.ball.2@gmail.com>
2016-03-14 22:35:46 -0700

    towards uploads

--- setbot-server/server.py
+++ setbot-server/server.py
@@ -1,13 +1,25 @@
+"""Setbot server.
+"""
+import os
+
 import flask
+from werkzeug import secure_filename
+
+upload_folder = '/tmp/setbot-uploads'
+allowed_extensions = ('png', 'jpg', 'jpeg')
+app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = upload_folder
 
 
-application = flask.Flask(__name__)
-
-
-@application.route("/")
+@app.route('/')
 def index():
-    return flask.render_template('index.html')
+  return flask.render_template('index.html')
 
 
-if __name__ == "__main__":
-    application.run(host='127.0.0.1', port=8888)
+@app.route('/uploads')
+def uploads():
+  pass
+
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8888)
--- setbot-server/wsgi.py
+++ setbot-server/wsgi.py
@@ -1,5 +1,5 @@
-from server import application
+from server import app
 
 
 if __name__ == "__main__":
-    application.run()
+  app.run()
