b49a4973d210effbb4524b3fba8efc39cd7b505b
Matt Ball <matt.ball.2@gmail.com>
2016-04-01 10:48:27 -0700

    adds a webcam test script

---
+++ camera.py
@@ -0,0 +1,30 @@
+"""Webcam test.
+
+via http://stackoverflow.com/a/19084592/232638
+"""
+
+import time
+import cv2
+
+
+cv2.namedWindow('preview')
+vc = cv2.VideoCapture(0)
+rval, frame = vc.read()
+
+while True:
+  # Show in preview window.
+  if frame is not None:
+     cv2.imshow('preview', frame)
+
+  # Save.
+  cv2.imwrite('/tmp/out.png', frame)
+
+  # Capture another.
+  rval, frame = vc.read()
+
+  # Break on 'q'.
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
+
+  # Wait.
+  time.sleep(2)
