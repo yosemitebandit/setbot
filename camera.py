eb66a0b1bc183f396a75b1ffc9054c93815c30f7
Matt Ball <matt.ball.2@gmail.com>
2016-04-07 01:28:40 -0700

    automatically detect number of cards

--- camera.py
+++ camera.py
@@ -6,12 +6,13 @@ import numpy as np
 
 
 cv2.namedWindow('preview')
-cv2.createTrackbar('sensitivity', 'preview', 80, 255, lambda x: x)
-cv2.createTrackbar('columns', 'preview', 3, 6, lambda x: x)
+cv2.createTrackbar('sensitivity', 'preview', 150, 255, lambda x: x)
 
 vc = cv2.VideoCapture(0)
 rval, frame = vc.read()
 
+area_difference_threshold = 0.2
+max_number_of_cards = 18
 cards_per_col = 3
 channels = 3
 aspect_ratio = 0.64
@@ -23,26 +24,42 @@ while True:
   # Show in preview window.
   if frame is not None:
 
-    # Get the number of columns from the slider.
-    cards_per_row = cv2.getTrackbarPos('columns', 'preview')
-    number_of_cards = cards_per_row * 3
-    new_image = np.zeros(
-      (height*cards_per_col, width*cards_per_row, channels), np.uint8)
-
     # Set threshold from slider.
     sensitivity = cv2.getTrackbarPos('sensitivity', 'preview')
     lower_white = np.array([0, 0, 255-sensitivity])
     upper_white = np.array([255, sensitivity, 255])
 
-    print 'sensitivity: %s, columns: %s' % (sensitivity, cards_per_row)
 
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     thresh = cv2.inRange(hsv_img, lower_white, upper_white)
 
     contours, _ = cv2.findContours(
       thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(
-      contours, key=cv2.contourArea, reverse=True)[0:number_of_cards]
+
+    first_six_contours = sorted(
+      contours, key=cv2.contourArea, reverse=True)[0:6]
+    areas = map(cv2.contourArea, first_six_contours)
+    # We use median here rather than average because that seems to be more
+    # resilient to encroaching hands.
+    median_area = np.median(areas)
+
+    possible_contours = sorted(
+      contours, key=cv2.contourArea, reverse=True)[0:max_number_of_cards]
+    contours = []
+    for contour in possible_contours:
+      diff = abs((cv2.contourArea(contour) - median_area) / median_area)
+      if diff < area_difference_threshold:
+        contours.append(contour)
+
+    number_of_cards = len(contours)
+    cards_per_row = number_of_cards / cards_per_col
+    max_number_of_cols = max_number_of_cards / cards_per_col
+    new_image = np.zeros(
+      (height*cards_per_col, width*max_number_of_cols, channels), np.uint8)
+    print 'sensitivity: %s, cards: %s' % (sensitivity, number_of_cards)
+
+    #contours = sorted(
+    #  contours, key=cv2.contourArea, reverse=True)[0:number_of_cards]
 
     # cv2.drawContours(frame, contours, -1, (0,255,0), 3)
 
